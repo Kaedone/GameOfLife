cf1bd6d9cc1c2f4427b7c77a34fdb9bfaf56dbcf
Тимофей Стабуль <timstab1@gmail.com>
2020-11-04 19:44:32 +0300

    Reset from PYgame, to the tkinter.

--- main.py
+++ main.py
@@ -1,53 +1,97 @@
+from tkinter import Tk, Canvas, Button, Frame, BOTH, NORMAL, HIDDEN
 
-import pygame
-from itertools import product, chain
 
-pygame.init()
-screen = pygame.display.set_mode((300, 300))
-clock = pygame.time.Clock()
-board = {(0,0),(1,0),(1,1),(0,1),(2,3),(2,2),(3,3),(3,2)}
-pygame.time.set_timer(pygame.USEREVENT, 1500)
+def draw_a(e):
+    ii = (e.y - 3) / cell_size
+    jj = (e.x - 3) / cell_size
+    canvas.itemconfig(cell_matrix[addr(ii, jj)], state=NORMAL, tags='vis')
 
-CELLSIZE = 32
-H_CELLSIZE = CELLSIZE / 2
 
-def neighbors(point):
-    x,y = point
-    for dx, dy in product((-1,0,1), repeat=2):
-        if dx != 0 or dy != 0:
-            if x + dx >= 0 and y + dy >= 0:
-                yield x + dx, y + dy
+def addr(ii, jj):
+    if ii < 0 or jj < 0 or ii >= field_height or jj >= field_width:
+        return int(len(cell_matrix) - 1)
+    else:
+        return int(ii * (win_width / cell_size) + jj)
 
-def step(board):
-    new_board = set()
-    with_neighbors = board | set(chain(*map(neighbors, board)))
-    for point in with_neighbors:
-        count = sum((neigh in board) for neigh in neighbors(point))
-        if count == 3 or (count == 2 and point in board):
-            new_board.add(point)
-    return new_board
 
-def scr_to_board(pos):
-    x, y = pos
-    return (x / CELLSIZE , y / CELLSIZE)
+def refresh():
+    for i in range(field_height):
+        for j in range(field_width):
+            k = 0
+            for i_shift in range(-1, 2):
+                for j_shift in range(-1, 2):
+                    if (canvas.gettags(cell_matrix[addr(i + i_shift, j + j_shift)])[0] == 'vis' and (
+                            i_shift != 0 or j_shift != 0)):
+                        k += 1
+            current_tag = canvas.gettags(cell_matrix[addr(i, j)])[0]
+            if k == 3:
+                canvas.itemconfig(cell_matrix[addr(i, j)], tags=(current_tag, 'to_vis'))
+            if k <= 1 or k >= 4:
+                canvas.itemconfig(cell_matrix[addr(i, j)], tags=(current_tag, 'to_hid'))
+            if k == 2 and canvas.gettags(cell_matrix[addr(i, j)])[0] == 'vis':
+                canvas.itemconfig(cell_matrix[addr(i, j)], tags=(current_tag, 'to_vis'))
 
-def board_to_scr(pos):
-    x, y = pos
-    return (x * CELLSIZE + H_CELLSIZE, y * CELLSIZE + H_CELLSIZE)
 
-while True:
-    if pygame.event.get(pygame.QUIT): 
-        break
+def step():
+    refresh()
+    repaint()
 
-    for e in pygame.event.get():
-        if e.type == pygame.MOUSEBUTTONDOWN:
-            board ^= {scr_to_board(pygame.mouse.get_pos())}
-        if e.type == pygame.USEREVENT:
-            board = step(board)
 
-    screen.fill(pygame.color.Color('white'))
-    for cell in board:
-        pygame.draw.circle(screen, pygame.color.Color('black'), board_to_scr(cell), H_CELLSIZE, 0)
+def clear():
 
-    pygame.display.flip()
-    clock.tick(60)
+    for i in range(field_height):
+        for j in range(field_width):
+            canvas.itemconfig(cell_matrix[addr(i, j)], state=HIDDEN, tags=('hid', '0'))
+
+
+def repaint():
+    for i in range(field_height):
+        for j in range(field_width):
+            if canvas.gettags(cell_matrix[addr(i, j)])[1] == 'to_hid':
+                canvas.itemconfig(cell_matrix[addr(i, j)], state=HIDDEN, tags=('hid', '0'))
+            if canvas.gettags(cell_matrix[addr(i, j)])[1] == 'to_vis':
+                canvas.itemconfig(cell_matrix[addr(i, j)], state=NORMAL, tags=('vis', '0'))
+
+
+root = Tk()
+root.title('4 cuba')
+win_width = int(350)
+win_height = int(370)
+config_string = "{0}x{1}".format(win_width, win_height + 32)
+fill_color = "green"
+root.geometry(config_string)
+cell_size = int(20)
+canvas = Canvas(root, height=win_height)
+canvas.pack(fill=BOTH)
+
+field_height = int(win_height / cell_size)
+field_width = int(win_width / cell_size)
+
+cell_matrix = []
+for i in range(field_height):
+    for j in range(field_width):
+        square = canvas.create_rectangle(2 + cell_size * j, 2 + cell_size * i, cell_size + cell_size * j - 2,
+                                         cell_size + cell_size * i - 2, fill=fill_color)
+        canvas.itemconfig(square, state=HIDDEN, tags=('hid', '0'))
+        cell_matrix.append(square)
+fictive_square = canvas.create_rectangle(0, 0, 0, 0, state=HIDDEN, tags=('hid', '0'))
+cell_matrix.append(fictive_square)
+
+canvas.itemconfig(cell_matrix[addr(8, 8)], state=NORMAL, tags='vis')
+canvas.itemconfig(cell_matrix[addr(10, 9)], state=NORMAL, tags='vis')
+canvas.itemconfig(cell_matrix[addr(9, 9)], state=NORMAL, tags='vis')
+canvas.itemconfig(cell_matrix[addr(9, 8)], state=NORMAL, tags='vis')
+canvas.itemconfig(cell_matrix[addr(9, 7)], state=NORMAL, tags='vis')
+canvas.itemconfig(cell_matrix[addr(10, 7)], state=NORMAL, tags='vis')
+
+frame = Frame(root)
+btn1 = Button(frame, text='Eval', command=step)
+btn2 = Button(frame, text='Clear', command=clear)
+btn1.pack(side='left')
+btn2.pack(side='right')
+frame.pack(side='bottom')
+
+canvas.bind('<B1-Motion>', draw_a)
+canvas.bind('<ButtonPress>', draw_a)
+
+root.mainloop()
